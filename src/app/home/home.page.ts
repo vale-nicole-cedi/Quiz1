8293f3aa7d690ad258208adae3e030a0a475c94f
vale-nicole-cedi <13047@portalcedi.edu.mx>
2025-01-23 09:21:31 -0600

    quizfinal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,29 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  evento = new FormControl('');
+  fechai = new FormControl('');
+  fechafin = new FormControl('');
+  ubicacion = new FormControl('');
+  encargado = new FormControl('');
+  telefono = new FormControl('');
 
-  constructor() {}
+  items: any = [];
+
+  agregar() {
+    if (this.evento.value && this.fechai.value && this.fechafin.value && this.ubicacion.value && this.encargado.value && this.telefono.value) {
+      this.items.push({ evento: this.evento.value, fechai: this.fechai.value, fechafin: this.fechafin.value, ubicacion: this.ubicacion.value, encargado: this.encargado.value, telefono: this.telefono.value });
+      this.evento.setValue('');
+      this.fechai.setValue('');
+      this.fechafin.setValue('');
+      this.ubicacion.setValue('');
+      this.encargado.setValue('');
+      this.telefono.setValue('');
+    }
+    else {
+      alert('Complete los datos requeridos');
+    }
+  }
+  constructor() { }
 
 }
